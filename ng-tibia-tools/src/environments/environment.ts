33149943184bceaf1153b6ca32bea47bf1632851
Carmo, Fernando <fernando_carmo500@hotmail.com>
2025-10-09 23:19:36 -0300

    secrets full fodase

--- ng-tibia-tools/src/environments/environment.ts
+++ ng-tibia-tools/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
+        apiKey: "AIzaSyD8M-xvVOxVVuX_kELqJEdd2dbVkAGCoJo",
+        authDomain: "utori-c6763.firebaseapp.com",
+        projectId: "utori-c6763",
+        storageBucket: "utori-c6763.firebasestorage.app",
+        messagingSenderId: "1086820738333",
+        appId: "1:1086820738333:web:186c55271e4f99bfbfedf4",
     }
 };
